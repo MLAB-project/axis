b13e79846e3eccc5d0135fb29641c5b15bd62d0f
Roman Dvořák <romandvorak@mlab.cz>
2017-07-17 08:12:55 +0200

    Calculate absolute data

--- examples/i2c_example_readStatus.py
+++ examples/i2c_example_readStatus.py
@@ -4,6 +4,7 @@
 import axis
 from pymlab import config
 import time
+import json
 
 import logging 
 logging.basicConfig(level=logging.INFO) 
@@ -29,12 +30,12 @@ spi.SPI_config(spi.I2CSPI_MSB_FIRST| spi.I2CSPI_MODE_CLK_IDLE_HIGH_DATA_EDGE_TRA
 
 motor = axis.axis(SPI = spi, SPI_CS = spi.I2CSPI_SS0, StepsPerUnit=200)
 #motor.Reset(KVAL_RUN = 0x20, KVAL_ACC = 0x20, KVAL_DEC = 0x20, FS_SPD = 0x0, stall_th = 203)
-motor.setConfig(F_PWM_INT = None, F_PWM_DEC = None, POW_SR = None, OC_SD = None, RESERVED = None, EN_VSCOMP = None, SW_MODE = None, EXT_CLK = None, OSC_SEL = None)
-motor.Setup(MAX_SPEED = 2000, KVAL_ACC=0.8, KVAL_RUN=0.3, ACC = 100, DEC = 100, FS_SPD=3000)
+#motor.setConfig(F_PWM_INT = None, F_PWM_DEC = None, POW_SR = None, OC_SD = None, RESERVED = None, EN_VSCOMP = None, SW_MODE = None, EXT_CLK = None, OSC_SEL = None)
+#motor.Setup(MAX_SPEED = 2000, KVAL_ACC=0.8, KVAL_RUN=0.3, ACC = 100, DEC = 100, FS_SPD=3000)
 
 #motor.setConfig(EXT_CLK = 0b1, OSC_SEL = 0b110) # Ext clock source: 24 MHz(Crystal/resonator driver disabled)
 
-motor.Float()
+#motor.Float()
 print motor.getStatus()
 
 
@@ -44,17 +45,5 @@ print motor.getStatus()
 #time.sleep(5)
 
 while True:
-
-    print motor.getStatus()
-    motor.Float()
-    print motor.getStatus()
-    motor.MoveWait(20)
-    print motor.getStatus()
-    time.sleep(10)
-    motor.Float()
-    motor.MoveWait(-20)
-    time.sleep(10)
-
-motor.GoToDir(20000)
-motor.wait()
-motor.GoTo(0)
+    print json.dumps(motor.getStatus(), indent=4, sort_keys=True)
+    time.sleep(0.10)
